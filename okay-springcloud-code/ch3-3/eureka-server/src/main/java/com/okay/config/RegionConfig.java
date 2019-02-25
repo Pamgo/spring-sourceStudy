d1d6cb3e4825b1b4d63a0901a5ca2afd022188dd
pamgo <pamgogdpyc@163.com>
2019-02-25 22:10:13 +0800

    支持Remote Region配置案例Server端

---
+++ okay-springcloud-code/ch3-3/eureka-server/src/main/java/com/okay/config/RegionConfig.java
@@ -0,0 +1,32 @@
+package com.okay.config;
+
+import com.netflix.discovery.EurekaClientConfig;
+import com.netflix.eureka.EurekaServerConfig;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.netflix.eureka.server.EurekaServerAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EurekaServerConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+
+/**
+ * Created by OKali on 2019/2/25.
+ */
+@Configuration
+@AutoConfigureBefore(EurekaServerAutoConfiguration.class)
+public class RegionConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public EurekaServerConfig eurekaServerConfig(EurekaClientConfig clientConfig) {
+        EurekaServerConfigBean server = new EurekaServerConfigBean();
+        if (clientConfig.shouldRegisterWithEureka()) {
+            server.setRegistrySyncRetries(5);
+        }
+        server.setRemoteRegionAppWhitelist(new HashMap<>());
+        return server;
+    }
+}
