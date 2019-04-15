110a1f8f85935d20b654e8470f4a902c26338d86
pamgo <pamgogdpyc@163.com>
2019-04-15 21:37:17 +0800

    生产者消费者

---
+++ okay-springcloud-code/ch-java8Test/src/main/java/com/okay/thread/finalPa/Product.java
@@ -0,0 +1,7 @@
+package com.okay.thread.finalPa;
+
+/**
+ * Created by OKali on 2019/4/15.
+ */
+public class Product {
+}
---
+++ okay-springcloud-code/ch-java8Test/src/main/java/com/okay/thread/producerAndConsumer/Producer.java
@@ -0,0 +1,7 @@
+package com.okay.thread.producerAndConsumer;
+
+/**
+ * Created by OKali on 2019/4/15.
+ */
+public class Producer {
+}
