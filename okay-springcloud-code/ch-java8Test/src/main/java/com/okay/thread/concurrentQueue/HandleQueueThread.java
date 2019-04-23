bdabe2b9f6a3eec20845ac92ef445a50c9bcda50
pamgo <pamgogdpyc@163.com>
2019-04-23 21:11:35 +0800

    回调

---
+++ okay-springcloud-code/ch-java8Test/src/main/java/com/okay/thread/concurrentQueue/HandleQueueThread.java
@@ -0,0 +1,7 @@
+package com.okay.thread.concurrentQueue;
+
+/**
+ * Created by OKali on 2019/4/21.
+ */
+public class HandleQueueThread {
+}
---
+++ okay-springcloud-code/ch-java8Test/src/main/java/com/okay/thread/mycallback/Test.java
@@ -0,0 +1,7 @@
+package com.okay.thread.mycallback;
+
+/**
+ * Created by OKali on 2019/4/23.
+ */
+public class Test {
+}
